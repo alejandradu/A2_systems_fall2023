0c8d59263d168ade0690d5340d9332f25ab09a2b
Alejandra Duran <ad2002@princeton.edu>
2023-09-30 16:01:01 -0400

    comments

--- stra.c
+++ stra.c
@@ -3,9 +3,20 @@
 #include <assert.h>  
 #include "str.h"
 
-/* Description */
-/* this argument does the assignment src[] = "input string" so LEN not necessary 
-will call this after: only works if they are null-terminated only once */
+/*  QUESTION: how is the length of char arrays, or arrays in general, limited? 
+
+QUESTION: can't use sizeof, have to call other, but that requires const,
+do i do a temp? 
+
+QUESTION: ASCII continuity */
+
+
+ /*  Get length of a string up to the first NULL                                                                                                                                                                                                                                                                               
+ *                                                                                                                                                                                        
+ *   src: null-terminated char array                                                                                                                                 
+ *                                                                                                                                                                                        
+ *   returns: number of chars before NULL (long unsigned int) */
+
 size_t Str_getLength(const char src[])
 {
    size_t uLength = 0;
@@ -15,29 +26,24 @@ size_t Str_getLength(const char src[])
    return uLength;
 }
 
-/*  QUESTION: how is the length of char arrays, or arrays in general, limited? 
-QUESTION: When i call the function: Str_copy(destination, copy) can 
-I pass arguments that have only been declared and not
-initialized? Ex char dest[]; 
-
-QUESTION: can't use sizeof, have to call other, but that requires const,
-do i do a temp?
-
-REMEMBER that we put the const to not modify the first src
-
-ASSUME that the out is big enough */
+ /*  Copy one string literal to another                                                                                                                                                                                                                                                                                
+ *                                                                                                                                                                        
+ *   src: null-terminated char array     
+ *   out: char array at least as big as src, where src is copied                                                                                                                           
+ *                                                                                                                                                                                        
+ *   returns: array (pointer to the first element) with src copy */
 
 char * Str_copy(char out[], const char src[])
 {
-   size_t LenOut = Str_getLength(out), i = 0;    /*sizes will include + 1 of last null*/
+   size_t LenOut = Str_getLength(out), i = 0;
    assert(src != NULL); 
    assert(out != NULL);
 
-   while(src[i] != '\0') {      /* copy all chars from the source up until first null*/
+   while(src[i] != '\0') {      /* copy all chars up until first null*/
       out[i] = src[i];
       i++;
    }
-   while(i < LenOut) {          /* add final null and fill any overhang */   
+   while(i <= LenOut) {          /* add final null and fill any overhang BUG */   
       out[i] = '\0';
       i++;
    }
@@ -45,15 +51,14 @@ char * Str_copy(char out[], const char src[])
 
 }
 
-/* ASSUME that out is big enough 
-as the book says with arrays there is no other way
-than traversing string until finding \n to know length 
-concat up until the first \n of src
-
-- it has to overwrite the last \n in the out string
-- for now ASSUMING that we take the first null in out after
-which we concat
-*/
+ /*  Concatenate one string literal to another                                                                                                                                                                                                                                                                                
+ *   Overwrites the (first) NULL termination of the initial
+ *   char sequence in out. Adds src immediately after.
+ *                                                                                                                                                                     
+ *   src: null-terminated char array     
+ *   out: null-terminated char array with enough empty elements to fit src                                                                                                                          
+ *                                                                                                                                                                                        
+ *   returns: array (pointer to the first element) with concatenation */
 
 char * Str_concat(char out[], const char src[])
 {
@@ -65,11 +70,11 @@ char * Str_concat(char out[], const char src[])
       i++;
    }
    while(src[j] != '\0') {      /* copy src up until first NULL */
-      out[i] = src[j];          /* overwrite last NULL of out */
+      out[i] = src[j];          
       j++;
       i++;
    }
-   while(i < LenOut) {          /* add final null and fill any overhang */   
+   while(i <= LenOut) {          /* add final null and fill any overhang BUG */   
       out[i] = '\0';
       i++;
    }
@@ -77,8 +82,15 @@ char * Str_concat(char out[], const char src[])
 
 }
 
-/* Do we need to worry abt portability - bc the int conversions
- of chars might not be continuous elsewhere, it might just work for ASCII */
+ /*  Compare two string literals                                                                                                                                                                                                                                                                               
+ *   Uses the continuity of ASCII representation
+ *   to compare chars with logical operators.
+ *                                                                                                                                                                     
+ *   s1: null-terminated char array     
+ *   s2: null-terminated char array                                                                                                                          
+ *                                                                                                                                                                                        
+ *   returns: (int) 0 if s1 == s2, 1 if s1 > s2, -1 if s2 > s1 */
+
 int Str_compare(const char s1[], const char s2[])
 {
    size_t i = 0;
@@ -86,18 +98,17 @@ int Str_compare(const char s1[], const char s2[])
    assert(s1 != NULL);
    assert(s2 != NULL);
 
-   while((c1 = s1[i]) == (c2 = s2[i])) {
-      i++;  
-      if(i == Str_getLength(s1) || i == Str_getLength(s2)) { /* is this redundant */
-         break;
+   while((c1 = s1[i]) == (c2 = s2[i])) {  
+      if(i == Str_getLength(s1) || i == Str_getLength(s2)) {    /* all chars are equal */
          return 0;
-      }  /* leaves i at the difference char*/
+      } 
+      i++;
    }
-      if(c1 == '\0') {  /* s1 finished first - s1 is shorter*/
+      if(c1 == '\0') {            /* s1 is shorter*/
          return -1;
-      } else if(c2 == '\0') {  /* s1 is longer*/
+      } else if(c2 == '\0') {     /* s1 is longer*/
          return 1;
-      } else {    /* the chars are just different  */
+      } else {                    /* the not-NULL chars are different  */
          if(c1 > c2) {
             return 1;
          } else {
@@ -106,27 +117,40 @@ int Str_compare(const char s1[], const char s2[])
       }
 }
 
+ /*  Helper function                                                                                                                                                                                                                                                                               
+ *   Determine if all chars of the needle match to the haystack starting at a given index
+ *                                                                                                                                                                     
+ *   haystack: null-terminated char array, at least as big as needle     
+ *   needle: null-terminated char array   
+ *   start: given index to start matching at haystack[start]      
+ *   len_needle: size of the needle, excluding the final NULL                                                                                                               
+ *                                                                                                                                                                                        
+ *   returns: (int) 0 if all needle chars match, 1 otherwise */
 
-/* helper function 
-find if all chars of the needle match starting at a given index in the haystack
-return 1 if true 0 if not*/
 int scan_match(const char haystack[], const char needle[], size_t start, size_t len_needle) {
    size_t k = 0;
-   while(k < len_needle) {
-      if(haystack[start + k] != needle[k]) { /* val of k is less than len*/
+   while(k <= len_needle) {
+      if(haystack[start + k] != needle[k]) { 
          break;
       }
       k++;
    }
-   if(k == len_needle - 1) { /* all chars matched*/
+   if(k == len_needle) {        /* all chars matched*/
       return 1;
    } else {
       return 0;
    }
 }
 
-/* assume that haystack is big enough, also that there is only one NULL in haystack? 
-assume null-terminated haystack and needle */
+ /*  Search for the first match of a given string literal (the needle)
+*    in another string literal (the haystack)
+ *                                                                                                                                                                     
+ *   haystack: null-terminated char array, at least as big as needle     
+ *   needle: null-terminated char array                                                                                                              
+ *                                                                                                                                                                                        
+ *   returns: NULL if needle is empty, haystack if no matches, or
+ *            pointer to the first element in haystack of match found */
+
 char * Str_search(const char haystack[], const char needle[])
 {
    size_t HLen = Str_getLength(haystack), NLen = Str_getLength(needle), i = 0;
@@ -134,20 +158,18 @@ char * Str_search(const char haystack[], const char needle[])
    assert(haystack != NULL);
    assert(needle != NULL);
 
-   if(NLen == 1) {  /* if needle is empty*/
+   if(NLen == 0) {                        /* if needle is empty*/
       return haystack;
    }
    else {
-      for(; i < HLen; i++) {
-         if(haystack[i] == needle[i]) { /* detect first match - find match*/
-            match = scan_match(haystack, needle, i, NLen);
+      for(; i <= HLen; i++) {
+         if(haystack[i] == needle[i]) {   /* first match */
+            match = scan_match(haystack, needle, i, NLen);    /* scan for needle match starting at i */
             if(match == 1) {
-               return &haystack[i];  /* returns adress of first char of match, also breaks out of loop*/
+               return &haystack[i]; 
             }
          }
-      } /* if no successful match*/
+      }                                   /* if no successful match*/
       return NULL;
    }
 }
-
-/* WARNING: return discards 'const' qualifier from pointer target type*/
