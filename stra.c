c3ed8309492f4a08526f8259df2e0c76eb0256b9
Alejandra Duran <ad2002@princeton.edu>
2023-09-30 14:41:44 -0400

    first warnings

--- stra.c
+++ stra.c
@@ -4,7 +4,8 @@
 #include "str.h"
 
 /* Description */
-// this argument does the assignment src[] = "input string" so LEN not necessary
+/* this argument does the assignment src[] = "input string" so LEN not necessary 
+will call this after: only works if they are null-terminated only once */
 size_t Str_getLength(const char src[])
 {
    size_t uLength = 0;
@@ -27,7 +28,7 @@ char * Str_copy(char out[], const char src[])
 {
    assert(src != NULL); 
    assert(out != NULL);
-   size_t LenOut = sizeof(out), i = 0;    // sizes will include + 1 of last null
+   size_t LenOut = Str_getLength(out), i = 0;    /*sizes will include + 1 of last null*/
 
    while(src[i] != '\0') {      /* copy all chars from the source up until first null*/
       out[i] = src[i];
@@ -55,7 +56,7 @@ char * Str_concat(char out[], const char src[])
 {
    assert(src != NULL);
    assert(out != NULL);
-   size_t LenOut = sizeof(out), i = 0, j = 0;
+   size_t LenOut = Str_getLength(out), i = 0, j = 0;
 
    while(out[i] != '\0') {      /* Get index of first NULL in out */
       i++;
@@ -84,16 +85,16 @@ int Str_compare(const char s1[], const char s2[])
 
    while((c1 = s1[i]) == (c2 = s2[i])) {
       i++;  
-      if(i == sizeof(s1) || i == sizeof(s2)) { // is this redundant
+      if(i == Str_getLength(s1) || i == Str_getLength(s2)) { /* is this redundant */
          break;
          return 0;
-      }  // leaves i at the difference char
+      }  /* leaves i at the difference char*/
    }
-      if(c1 == '\0') {  // s1 finished first - s1 is shorter
+      if(c1 == '\0') {  /* s1 finished first - s1 is shorter*/
          return -1;
-      } else if(c2 == '\0') {  // s1 is longer
+      } else if(c2 == '\0') {  /* s1 is longer*/
          return 1;
-      } else {    // the chars are just different  // 
+      } else {    /* the chars are just different  */
          if(c1 > c2) {
             return 1;
          } else {
@@ -107,14 +108,14 @@ int Str_compare(const char s1[], const char s2[])
 find if all chars of the needle match starting at a given index in the haystack
 return 1 if true 0 if not*/
 int scan_match(const char haystack[], const char needle[], size_t start, size_t len_needle) {
-   int k = 0;
+   size_t k = 0;
    while(k < len_needle) {
-      if(haystack[start + k] != needle[k]) { // val of k is less than len
+      if(haystack[start + k] != needle[k]) { /* val of k is less than len*/
          break;
       }
       k++;
    }
-   if(k == len_needle - 1) { // all chars matched
+   if(k == len_needle - 1) { /* all chars matched*/
       return 1;
    } else {
       return 0;
@@ -127,21 +128,23 @@ char * Str_search(const char haystack[], const char needle[])
 {
    assert(haystack != NULL);
    assert(needle != NULL);
-   size_t HLen = sizeof(haystack), NLen = sizeof(needle), i = 0;
+   size_t HLen = Str_getLength(haystack);
+   size_t NLen = Str_getLength(needle);
+   size_t i = 0;
    int match;
 
-   if(NLen == 1) {  // if needle is empty
+   if(NLen == 1) {  /* if needle is empty*/
       return haystack;
    }
    else {
       for(; i < HLen; i++) {
-         if(haystack[i] == needle[i]) { // detect first match - find match
+         if(haystack[i] == needle[i]) { /* detect first match - find match*/
             match = scan_match(haystack, needle, i, NLen);
             if(match == 1) {
-               return haystack[i];  // returns adress of first char of match, also breaks out of loop
+               return &haystack[i];  /* returns adress of first char of match, also breaks out of loop*/
             }
          }
-      } // if no successful match
+      } /* if no successful match*/
       return NULL;
    }
 }
