32eb84bf6937beb07895bfc0867a6120e373a654
Alejandra Duran <ad2002@princeton.edu>
2023-09-30 16:26:55 -0400

    fix

--- str.h
+++ str.h
@@ -1,24 +1,26 @@
+/* header file for the Str module*/
+
 #ifndef str
 #define str
 
 #include <stddef.h>
 
-/* Comment */
+/* Comment src, function*/
 size_t Str_getLength(const char src[]);
 
-/* Comment */
+/* Comment out, src, function*/
 char * Str_copy(char out[], const char src[]);
 
-/* Comment */
+/* Comment out, src, function*/
 char * Str_concat(char out[], const char src[]);
 
-/* Comment */
+/* Comment s1, s2, function*/
 int Str_compare(const char s1[], const char s2[]);
 
-/* Comment */
+/* Comment haystack, needle, start, len_needle, function*/
 int Str_scan_match(const char haystack[], const char needle[], size_t start, size_t len_needle);
 
-/* Comment */
+/* Comment haystack, needle, function*/
 char * Str_search(const char haystack[], const char needle[]);
 
 #endif
