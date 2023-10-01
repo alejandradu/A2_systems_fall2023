3c9451871b52cf0498b8f4f7279453b34d4b7325
Alejandra Duran <ad2002@princeton.edu>
2023-09-30 22:52:12 -0400

    try

--- strp.c
+++ strp.c
@@ -5,7 +5,7 @@
 
 size_t Str_getLength(const char *src)
 {
-   const char *end;
+   const char *end;   /* what is const in here? in the end i do ++*/
    assert(src != NULL);
    end = src;
    while (*end != '\0')
@@ -15,37 +15,34 @@ size_t Str_getLength(const char *src)
 
 char * Str_copy(char *out, const char *src)
 {
-   size_t LenOut = Str_getLength(out);
-   char *curr = src;   /* pointer to src DON'T modify src value QUESTION or &?*/
+   const char *curr;        /* do this bc i don't wanna change src? */
    assert(src != NULL); 
    assert(out != NULL);
+   curr = src;
 
-   while(*src != '\0') {      /* copy all chars up until first null*/
-      *out = *curr;   /* should initially be the same value of src? */
+   while(*curr != '\0') {    /* copy all chars up until first null*/
+      *out = *curr;   
       curr++;
       out++;
    }
-   while(curr - src != LenOut) {   /* add final null and fill any overhang BUG */
-      *out = '\0';
-      curr++;
-      out++;                       /* in the end out has moved LenOut*/
-   }
-   return out - (int) LenOut;      /* should be pointer to memory adress */
+   *out = '\0';              /* add final null */
+ 
+   return out - (curr - src);      /* curr - src is len out has moved */
 
 }
 
 char * Str_concat(char *out, const char *src)
 {
    size_t LenOut = Str_getLength(out), i = 0, j = 0;
-   char *curr = src;
+   const char *curr = src;
    assert(src != NULL);
    assert(out != NULL);
 
    while(*out != '\0') {      /* Get index of first NULL in out */
       i++;
    }
-   while(src[j] != '\0') {      /* copy src up until first NULL */
-      out[i] = src[j];          
+   while(curr[j] != '\0') {      /* copy src up until first NULL */
+      out[i] = curr[j];          
       j++;
       i++;
    }
