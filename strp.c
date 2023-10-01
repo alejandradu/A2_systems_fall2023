52baead57c992a39f596a38ec4660c700db873c0
Alejandra Duran <ad2002@princeton.edu>
2023-09-30 23:45:57 -0400

    fix currs

--- strp.c
+++ strp.c
@@ -87,15 +87,17 @@ int Str_compare(const char *s1, const char *s2)
 
 int Str_scan_match(const char *curr_haystack, const char *needle, size_t dummy) {
    const char *curr_needle;
+   const char *curr_temp_haystack;
    assert(curr_haystack != NULL);
    assert(needle != NULL);
    curr_needle = needle;
+   curr_temp_haystack = curr_haystack;
 
    while(*needle != '\0') {
-      if(*curr_haystack != *curr_needle) { 
+      if(*curr_temp_haystack != *curr_needle) { 
          break;
       }
-      curr_haystack++;
+      curr_temp_haystack++;
       curr_needle++;
    }
    if(*curr_needle == '\0') {        /* all chars matched */
@@ -115,14 +117,14 @@ char * Str_search(const char *haystack, const char *needle)
    curr = haystack;
 
    if(*needle == '\0') {                 /* if needle is empty*/
-      return curr;               /* cast */
+      return (char*) curr;               /* cast */
    }
    else {
       while(*curr != '\0') {
          if(*curr == *needle) {    
             match = Str_scan_match(curr, needle, dummy);    /* scan for needle match starting at i */
             if(match == 1) {
-               return curr;      /* cast */
+               return (char*) curr;      /* cast */
             }
          }
          curr++;   
