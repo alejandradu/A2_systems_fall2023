0517e8d4ca8297ef6289eec76bd3576663eac0a4
Alejandra Duran <ad2002@princeton.edu>
2023-10-05 19:07:22 -0400

    print

--- replace.c
+++ replace.c
@@ -34,24 +34,25 @@ static size_t replaceAndWrite(const char *pcLine,
 
    start_match = pcLine;       /* assign at the beginnind of string */
    *temp = Str_search(start_match, pcFrom);    /* correct assignment? */
+   printf("address of match char: %p\n", temp);
 
    if(temp == NULL) {    /* pcFrom is NULL or no matches*/
       printf(pcLine);
       return 0;
    }
 
-   printf("address of match char: %p\n", temp);
    printf("address of given line: %p\n", pcLine);
    printf("address of start_match: %p\n", start_match);
-   /*while(temp != start_match) {   /* no matches when it returns the same input */
-   /*   /*printf("value of match char: %c\n", *temp);*/
-   /*   printf("scanning\n");
-   /*   count++;
-   /*   /* check it does not go over the length */
-   /*   start_match = temp;      /* shift pointer to start a new search BUG + or minus 1??*/
-   /*   /* CHECK NOT TO GO OVER LENGTH - BUG*/
-   /*   *temp = Str_search(start_match, pcFrom);     /* new search */
-   /*}
+
+   while(temp != start_match) {   /* no matches when it returns the same input */
+      /*printf("value of match char: %c\n", *temp);*/
+      printf("scanning\n");
+      count++;
+      /* check it does not go over the length */
+      start_match = temp;      /* shift pointer to start a new search BUG + or minus 1??*/
+      /* CHECK NOT TO GO OVER LENGTH - BUG*/
+      *temp = Str_search(start_match, pcFrom);     /* new search */
+   }
 
    /*char result[LineLen + count*ToLen + 1];     /* output array */
    /*start_match = pcLine;                       /* reset pointers */
